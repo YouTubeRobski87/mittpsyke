e242eabe852b6d76eac6ea617cc34b3b428908e8
Robski87 <rbsthh@gmail.com>
2026-02-09 08:06:29 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const useNodeAdapterLocally =
+	process.platform === 'win32' && !process.env.CI && !process.env.VERCEL;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ runtime: 'nodejs22.x' })
+		adapter: useNodeAdapterLocally
+			? adapterNode()
+			: adapter({ runtime: 'nodejs22.x' })
 	}
 };
 
