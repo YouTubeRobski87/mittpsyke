83cc4610a400622beeb2930e6a94c6e482884d62
Robski87 <rbsthh@gmail.com>
2026-03-07 13:32:06 +0100

    Fix page.server.ts locals.supabase

--- src/routes/framsteg/+page.server.ts
+++ src/routes/framsteg/+page.server.ts
@@ -1,19 +1,14 @@
-// src/routes/journey/+page.server.ts
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	// Kontrollera att användaren är inloggad
 	const {
 		data: { session }
 	} = await locals.supabase.auth.getSession();
 
 	if (!session) {
-		throw redirect(303, '/auth');
+		throw redirect(303, '/login');
 	}
-
-	// Vi behöver inte förhämta data här eftersom komponenten gör det via API
-	// Men vi kan lägga till användardata om det behövs senare
 
 	return {
 		session
