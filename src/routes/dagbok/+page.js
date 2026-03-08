9140e10ff03dc2356085f6e38f20047853aa11f7
Robski87 <rbsthh@gmail.com>
2026-03-08 11:32:53 +0100

    SEO: Uppdaterad title/description för /dagbok

--- src/routes/dagbok/+page.js
+++ src/routes/dagbok/+page.js
@@ -1,6 +1,6 @@
 export const load = () => {
   return {
-    title: "Digitala verktyg mot ångest – Din personliga dagbok",
+    title: "Verktyg mot ångest online – Din personliga dagbok",
     description: "Mittpsyke erbjuder digitala verktyg mot ångest online. Skriv din personliga dagbok och följ ditt mående med en visuell dashboard."
   };
 };
