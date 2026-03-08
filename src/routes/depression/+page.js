b1aaedcdd76dab1cae5f03f1b937ffa31cb532d4
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:32 +0100

    SEO: +page.js för /depression med unik title och description

---
+++ src/routes/depression/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Stöd vid depression online – Bearbeta i din egen takt",
+    description: "MittPsyke erbjuder anonymt stöd vid depression online. Digitala verktyg och samtalsstöd för dig som mår dåligt."
+  };
+};
