6288be4a30fa2bdb8e83857856a1489e2d9b6dd8
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:29 +0100

    SEO: +page.js för /stress med unik title och description

---
+++ src/routes/stress/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Stöd vid stress online – Hantera stressen digitalt",
+    description: "Få professionellt stöd vid stress online via MittPsyke. Anonyma verktyg och samtalsstöd för dig som känner dig överväldigad."
+  };
+};
