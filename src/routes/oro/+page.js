472bd7c2adf358722dbfd87e325d6e6fda0832a5
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:29 +0100

    SEO: +page.js för /oro med unik title och description

---
+++ src/routes/oro/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Hjälp mot oro online – Lugn och verktyg för din vardag",
+    description: "MittPsyke erbjuder hjälp mot oro online. Beprövade metoder och anonymt stöd för dig som kämpar med ständig oro."
+  };
+};
