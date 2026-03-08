967a3d3a762eb0bd4a397df28bd301c783f8a783
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:33 +0100

    SEO: +page.js för /om-mittpsyke med unik title och description

---
+++ src/routes/om-mittpsyke/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Om MittPsyke – Din trygga plats för psykiskt stöd",
+    description: "Lär dig mer om MittPsyke, vår vision och hur vi erbjuder anonymt psykiskt stöd online för alla."
+  };
+};
