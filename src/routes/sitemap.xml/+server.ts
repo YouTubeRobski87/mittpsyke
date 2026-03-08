69660cfd9361ae116693fcdf9333a192d0e93c31
Robski87 <rbsthh@gmail.com>
2026-03-08 11:50:03 +0100

    Fix: Uppdatera sitemap +server.ts med alla sidor

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,13 +1,35 @@
 import { getGuiderSeoPaths } from '$lib/seo-kit/content';
 import { canonical } from '$lib/seo-kit/seo';
 
+const staticPages = [
+	'',
+	'trauma',
+	'dagbok',
+	'angest',
+	'depression',
+	'stress',
+	'oro',
+	'nedstamdhet',
+	'ensamhet',
+	'panikattack',
+	'om-mittpsyke',
+];
+
 export function GET() {
-	const urls = getGuiderSeoPaths()
-		.map((path) => `<url><loc>${canonical(path)}</loc></url>`)
+	const dynamicUrls = getGuiderSeoPaths()
+		.map((path) => `<url><loc>${canonical(path)}</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>`)
+		.join('');
+
+	const staticUrls = staticPages
+		.map((page) => {
+			const loc = `https://mittpsyke.se/${page}`;
+			const priority = page === '' ? '1.0' : '0.8';
+			return `<url><loc>${loc}</loc><changefreq>weekly</changefreq><priority>${priority}</priority></url>`;
+		})
 		.join('');
 
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${staticUrls}${dynamicUrls}</urlset>`;
 
 	return new Response(xml, {
 		headers: {
