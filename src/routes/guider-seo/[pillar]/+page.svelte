d662102ab3f4eeec400b3839c8d39412ff938c77
Robski87 <rbsthh@gmail.com>
2026-03-08 11:53:06 +0100

    feat: add JSON-LD Schema Markup to [pillar]

--- src/routes/guider-seo/[pillar]/+page.svelte
+++ src/routes/guider-seo/[pillar]/+page.svelte
@@ -3,12 +3,27 @@
 	import type { PageData } from './$types';
 
 	let { data } = $props<{ data: PageData }>();
+
+	$: jsonLd = {
+		"@context": "https://schema.org",
+		"@type": "CollectionPage",
+		"headline": data.pillar.title,
+		"description": data.pillar.description,
+		"url": `https://mittpsyke.se/guider-seo/${data.pillar.slug}`,
+		"author": {
+			"@type": "Organization",
+			"name": "MittPsyke",
+			"url": "https://mittpsyke.se"
+		},
+		"inLanguage": "sv-SE"
+	};
 </script>
 
 <svelte:head>
 	<title>{buildTitle(data.pillar.title)}</title>
 	<link rel="canonical" href={canonical(`/guider-seo/${data.pillar.slug}`)} />
 	<meta name="description" content={data.pillar.description} />
+	{@html `<script type="application/ld+json">${JSON.stringify(jsonLd)}</script>`}
 </svelte:head>
 
 <main>
