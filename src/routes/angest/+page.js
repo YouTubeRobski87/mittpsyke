74d59f7edc33d73c5395a56551f53224cee2790f
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:32 +0100

    SEO: +page.js för /angest med unik title och description

---
+++ src/routes/angest/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Hjälp vid ångest online – Verktyg och samtalsstöd",
+    description: "Få hjälp vid ångest online via MittPsyke. Anonyma verktyg, andningsövningar och samtalsstöd för din psykiska hälsa."
+  };
+};
