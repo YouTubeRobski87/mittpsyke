35778d17c56d8158242c6cbe0f7ff1e4b7b378bc
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:27 +0100

    SEO: +page.js för /trauma med unik title och description

---
+++ src/routes/trauma/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Samtalsstöd vid trauma",
+    description: "Få anonymt samtalsstöd vid trauma och PTSD. Vi erbjuder trygga verktyg för din återhämtning."
+  };
+};
