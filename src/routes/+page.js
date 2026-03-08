afed1f8bdece91dcee9956b281655ae1d229e19e
Robski87 <rbsthh@gmail.com>
2026-03-08 11:32:51 +0100

    SEO: Uppdaterad title/description för /

---
+++ src/routes/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Psykiskt stöd online – Din trygga plats för återhämtning",
+    description: "Mittpsyke erbjuder anonymt samtalsstöd online och digitala verktyg mot ångest och trauma. Starta din dagbok eller chatta med oss idag."
+  };
+};
