e678141de2e5aeea32034496dc6ddddadd46fc3b
Robski87 <rbsthh@gmail.com>
2026-03-08 11:31:30 +0100

    SEO: +page.js för /nedstamdhet med unik title och description

---
+++ src/routes/nedstamdhet/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+  return {
+    title: "Samtalsstöd för nedstämdhet – Du behöver inte bära det ensam",
+    description: "Få samtalsstöd för nedstämdhet online via MittPsyke. Anonymt, tryggt och tillgängligt dygnet runt."
+  };
+};
