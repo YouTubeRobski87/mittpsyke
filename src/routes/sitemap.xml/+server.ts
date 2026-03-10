c5ed2aec21a41c84589ad36dabcd499d70af36a4
Robski87 <rbsthh@gmail.com>
2026-03-10 07:48:52 +0100

    Fix sitemap static URLs

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,38 +1,40 @@
 import { getGuiderSeoPaths } from '$lib/seo-kit/content';
 import { canonical } from '$lib/seo-kit/seo';
 
-const staticPages = [
-	'',
-	'trauma',
-	'dagbok',
-	'psykiskt-stod-online',
-	'anonymt-samtalsstod-online',
-	'hjalp-vid-angest-online',
-	'hjalp-vid-depression-online',
-	'stod-vid-stress-online',
-	'hjalp-mot-oro-online',
-	'samtalsstod-vid-trauma',
-	'prata-anonymt-online',
-	'digital-dagbok-for-maende',
-	'samtalsstod-utan-vantetid',
-	'ai-samtalsstod-online',
-	'andningsovningar-mot-angest',
-	'ovningar-mot-angest-online',
-	'4-7-8-andning-ovning',
-	'exponering-ovningar-mot-angest',
-	'stod-vid-ptsd-online',
-	'anonym-dagbok-online',
-	'chatta-anonymt-med-nagon',
-	'chattstod-psykisk-ohalsa',
-	'angest',
-	'depression',
-	'stress',
-	'oro',
-	'nedstamdhet',
-	'ensamhet',
-	'panikattack',
-	'om-mittpsyke',
-];
+const staticPages = Array.from(
+	new Set([
+		'',
+		'trauma',
+		'dagbok',
+		'psykiskt-stod-online',
+		'anonymt-samtalsstod-online',
+		'hjalp-vid-angest-online',
+		'hjalp-vid-depression-online',
+		'stod-vid-stress-online',
+		'hjalp-mot-oro-online',
+		'samtalsstod-vid-trauma',
+		'prata-anonymt-online',
+		'digital-dagbok-for-maende',
+		'samtalsstod-utan-vantetid',
+		'ai-samtalsstod-online',
+		'andningsovningar-mot-angest',
+		'ovningar-mot-angest-online',
+		'4-7-8-andning-ovning',
+		'exponering-ovningar-mot-angest',
+		'stod-vid-ptsd-online',
+		'anonym-dagbok-online',
+		'chatta-anonymt-med-nagon',
+		'chattstod-psykisk-ohalsa',
+		'angest',
+		'depression',
+		'stress',
+		'oro',
+		'nedstamdhet',
+		'ensamhet',
+		'panikattack',
+		'om-mittpsyke'
+	])
+);
 
 export function GET() {
 	const dynamicUrls = getGuiderSeoPaths()
@@ -41,11 +43,13 @@ export function GET() {
 
 	const staticUrls = staticPages
 		.map((page) => {
-			const loc = `https://mittpsyke.se/${page}`;
-			const priority = page === '' ? '1.0' : '0.8';
-			return `<url><loc>${loc}</loc><changefreq>weekly</changefreq><priority>${priority}</priority></url>`;
-		})
-		.join('');
+		const loc = page === ''
+			? 'https://www.mittpsyke.se/'
+			: `https://www.mittpsyke.se/${page}`;
+		const priority = page === '' ? '1.0' : '0.8';
+		return `<url><loc>${loc}</loc><changefreq>weekly</changefreq><priority>${priority}</priority></url>`;
+	})
+	.join('');
 
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${staticUrls}${dynamicUrls}</urlset>`;
