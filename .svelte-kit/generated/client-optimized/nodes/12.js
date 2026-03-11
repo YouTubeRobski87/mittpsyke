3256efb08d220be2cbca21979c4d1e5a9cab751c
Robski87 <rbsthh@gmail.com>
2026-03-11 21:19:17 +0100

    fixes

--- .svelte-kit/generated/client-optimized/nodes/12.js
+++ .svelte-kit/generated/client-optimized/nodes/12.js
@@ -1,3 +1,1 @@
-import * as universal from "../../../../src/routes/guider/[pillar]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/guider/[pillar]/+page.svelte";
+export { default as component } from "../../../../src/routes/chat/[category]/+page.svelte";
