0d05590acc4efcfa781632b4fa85b6d0c9dac12b
Robski87 <rbsthh@gmail.com>
2026-03-13 20:33:12 +0100

    fix: markdownToHtml hanterar nu ### h3, - listor och blandad bold

--- src/routes/guider-seo/[pillar]/[guide]/+page.svelte
+++ src/routes/guider-seo/[pillar]/[guide]/+page.svelte
@@ -46,7 +46,11 @@
 
 	function markdownToHtml(md: string): string {
 		return md
+			.replace(/^### (.+)$/gm, '<h3 class="mt-5 text-lg font-semibold">$1</h3>')
 			.replace(/^## (.+)$/gm, '<h2 class="mt-7 text-xl font-semibold">$1</h2>')
+			.replace(/^- \*\*(.+?)\*\*(.*)$/gm, '<li class="ml-4 mt-1 leading-relaxed text-black/80"><strong>$1</strong>$2</li>')
+			.replace(/^- (.+)$/gm, '<li class="ml-4 mt-1 leading-relaxed text-black/80">$1</li>')
+			.replace(/(<li[^>]*>.*<\/li>\n?)+/g, (match) => `<ul class="mt-3 list-disc pl-4">${match}</ul>`)
 			.replace(/^\*\*(.+?)\*\*$/gm, '<p class="mt-3 font-semibold">$1</p>')
 			.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
 			.replace(/\n\n/g, '</p><p class="mt-3 leading-relaxed text-black/80">')
