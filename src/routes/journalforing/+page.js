980a711c6735dc5afe8169fc8b067fd7e9aa2901
Robski87 <rbsthh@gmail.com>
2026-03-19 22:03:06 +0100

    Create +page.js

---
+++ src/routes/journalforing/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+	return {
+		description:
+			'Forskning visar att regelbunden journalföring förbättrar mental hälsa. MittPsyke gör det enkelt med AI-guidat skrivande på svenska.'
+	};
+};
