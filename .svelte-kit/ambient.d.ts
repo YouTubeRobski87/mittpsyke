fbdbcc176b00cf58ecafb7ad19437698bab303cc
Robski87 <rbsthh@gmail.com>
2026-03-19 20:21:31 +0100

    Update ambient.d.ts

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -62,7 +62,6 @@ declare module '$env/static/private' {
 	export const LOGONSERVER: string;
 	export const NODE: string;
 	export const NODE_EXE: string;
-	export const NPM_CLI_JS: string;
 	export const npm_command: string;
 	export const npm_config_cache: string;
 	export const npm_config_globalconfig: string;
@@ -83,7 +82,8 @@ declare module '$env/static/private' {
 	export const npm_package_name: string;
 	export const npm_package_version: string;
 	export const NPM_PREFIX_JS: string;
-	export const NPM_PREFIX_NPM_CLI_JS: string;
+	export const NPM_PREFIX_NPX_CLI_JS: string;
+	export const NPX_CLI_JS: string;
 	export const NUMBER_OF_PROCESSORS: string;
 	export const NVIDIAWHITELISTED: string;
 	export const OneDrive: string;
@@ -230,7 +230,6 @@ declare module '$env/dynamic/private' {
 		LOGONSERVER: string;
 		NODE: string;
 		NODE_EXE: string;
-		NPM_CLI_JS: string;
 		npm_command: string;
 		npm_config_cache: string;
 		npm_config_globalconfig: string;
@@ -251,7 +250,8 @@ declare module '$env/dynamic/private' {
 		npm_package_name: string;
 		npm_package_version: string;
 		NPM_PREFIX_JS: string;
-		NPM_PREFIX_NPM_CLI_JS: string;
+		NPM_PREFIX_NPX_CLI_JS: string;
+		NPX_CLI_JS: string;
 		NUMBER_OF_PROCESSORS: string;
 		NVIDIAWHITELISTED: string;
 		OneDrive: string;
