e8e3e61477b3a40968a859dcf8cf9647c08824a4
Robski87 <rbsthh@gmail.com>
2026-03-19 22:03:09 +0100

    Create +page.js

---
+++ src/routes/humorsparning/+page.js
@@ -0,0 +1,6 @@
+export const load = () => {
+	return {
+		description:
+			'Följ dina känslor över tid med visuell humörspårning, streaks och insikter. Se mönster i ditt mående med MittPsyke.'
+	};
+};
