d47a816af0d5d596010fa4789997a6942c7222c7
Robski87 <rbsthh@gmail.com>
2026-03-29 16:00:31 +0200

    fix: redirect successful login to dashboard

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -33,7 +33,7 @@
 		const { error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				redirectTo: `${window.location.origin}/auth/callback?next=/min-portal`
+				redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`
 			}
 		});
 
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -43,7 +43,7 @@
 		const { error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				redirectTo: `${window.location.origin}/auth/callback?next=/min-portal`
+				redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`
 			}
 		});
 
