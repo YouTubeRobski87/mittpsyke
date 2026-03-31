fe3d1323f6f82eb6638ef5de2b1e9ea1a0b19e75
Robski87 <rbsthh@gmail.com>
2026-04-01 00:55:49 +0200

    Update PortalSubnav.svelte

--- src/lib/components/PortalSubnav.svelte
+++ src/lib/components/PortalSubnav.svelte
@@ -11,7 +11,7 @@
 		{ key: 'dashboard', label: 'Min portal', href: '/dashboard' },
 		{ key: 'dagbok', label: 'Dagbok', href: '/dagbok' },
 		{ key: 'framsteg', label: 'Framsteg', href: '/framsteg' },
-		{ key: 'gemenskap', label: 'Gemenskap', href: '/dashboard/gemenskap' },
+		{ key: 'gemenskap', label: 'Översikt', href: '/dashboard/gemenskap' },
 		{ key: 'installningar', label: 'Inställningar', href: '/dashboard/installningar' }
 	];
 
