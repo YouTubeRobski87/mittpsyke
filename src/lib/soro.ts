ac2e0a3d3343eb5586865b22af8b32925d29cbc1
Robski87 <rbsthh@gmail.com>
2026-05-31 22:53:43 +0200

    Update soro.ts

--- src/lib/soro.ts
+++ src/lib/soro.ts
@@ -1,2 +1,2 @@
-export const SORO_TOKEN = '8ddf4490-ef9e-45aa-998d-adfb8114dd7f';
+export const SORO_TOKEN = '9a08468b-7239-4e08-bd5f-5bf59e87b3f7';
 export const SORO_EMBED_SRC = `https://app.trysoro.com/api/embed/${SORO_TOKEN}?theme=dark`;
