0a7f93a568880597119718d72605e498407244f8
Robski87 <rbsthh@gmail.com>
2026-08-04 07:16:15 +0200

    Add blog post: kvällsångest (metadata)

---
+++ src/routes/blogg/kvallasangest/+page.js
@@ -0,0 +1,7 @@
+export const load = () => {
+	return {
+		title: 'Kvällsångest: varför den kommer just när du ska sova, och vad som faktiskt hjälper',
+		description:
+			'Varför blir ångesten värre på kvällen? Lär dig vad kvällsångest är, varför tankarna snurrar när det blir tyst, och konkreta sätt att lugna sinnet innan sömn.'
+	};
+};
